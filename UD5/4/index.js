777c2f0f206b3a12a4b46201d84170a6cec1ae19
Rut María Ayuso Comino <93192971+RutAyuso@users.noreply.github.com>
2021-11-20 11:33:43 +0100

    Delete UD5/4 directory

--- UD5/4/index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("entrada").addEventListener("click",function(){
-    var liañadido = document.createElement("li");
-    var textoli = document.createTextNode("nueva entrada");
-    
-    liañadido.appendChild(textoli);
-    document.getElementsByTagName("ul")[0].appendChild(liañadido);})
