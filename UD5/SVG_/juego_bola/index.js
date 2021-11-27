351af6a3e4d567835a68c46facc4a2f4407b252d
Rut <rutmariaayusocomino@gmail.com>
2021-11-27 20:51:27 +0100

    bolas

--- UD5/SVG_/juego_bola/index.js
+++ UD5/SVG_/juego_bola/index.js
@@ -27,76 +27,44 @@ class bola
         
         // Comprobamos ahora si está fuera de los límites
         // Eje X
-        if (this.x-this.radio<=0 || this.x+this.radio >= anchoContenedor )
-        {
+        if (this.x-this.radio<=0 || this.x+this.radio >= anchoContenedor ){
             this.vX *= -1;
             this.x  = (this.x-this.radio<=0) ? this.radio: anchoContenedor-this.radio;
         }
-        
         // Eje Y
-        if (this.y-this.radio <=0 || this.y+this.radio >= altoContenedor )
-        {
+        if (this.y-this.radio <=0 || this.y+this.radio >= altoContenedor ){
             this.vY *= -1;
             this.y  = (this.y-this.radio<=0) ? this.radio: altoContenedor-this.radio;
         }
     }
-
-    colisiona(otraBola){
-        let dx = this.x - otraBola.x;
-        let dy = this.y - otraBola.y;
-        let distancia = Math.sqrt(dx * dx + dy * dy);
-
-        if (distancia < this.radio + otraBola.radio) {
-            this.vX *=-1;
-            this.vY *= -1;
-
-            this.x = this.posAnteriorX;
-            this.y = this.posAnteriorY;
-        }
-    }
     
-    dibuja()
-    {
+    dibuja(){
         this.tagCircle.setAttribute("cx", this.x);
         this.tagCircle.setAttribute("cy", this.y);
     }
-
 }
 
-function aleatorio(min, max) {
+function aleatorio(min, max){
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function colorAleatorio()
-{
+function colorAleatorio(){
     return "#"+ Math.floor(Math.random()*16777215).toString(16);
 }
 
 var bolas = new Array();
 var svg; 
 
- window.onload = () =>
-  {
-      svg = document.getElementById("panel");
-
-      for (let i=0; i<15; i++)
+ window.onload = () =>{
+        svg = document.getElementById("panel");
         bolas.push(new bola(aleatorio(4,50), aleatorio(70,900), aleatorio(70,900), aleatorio(1,15),aleatorio(1,15),colorAleatorio(),svg));
-
-     // setInterval( loop, 30);
-     elId = window.requestAnimationFrame(loop);
+        elId = window.requestAnimationFrame(loop);
   }
 
-  function loop()
-  {
+  function loop(){
       tamanoSVG = svg.getBoundingClientRect();
-      for (let i=0; i<bolas.length; i++)
-      {
-        bolas[i].mueve(tamanoSVG.width, tamanoSVG.height);
-
-        for (let j=0; j<bolas.length; j++)
-            if (i!=j) bolas[i].colisiona(bolas[j]);
-        
-          bolas[i].dibuja();
-      }
+      for (let i=0; i<bolas.length; i++){
+        bolas[i].mueve(tamanoSVG.width, tamanoSVG.height);        
+        bolas[i].dibuja();}
       elId = window.requestAnimationFrame(loop);
   }
