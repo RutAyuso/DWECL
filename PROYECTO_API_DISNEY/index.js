93548e77259d3c639d43b8b666873a4e9d0171b1
Rut <rutmariaayusocomino@gmail.com>
2021-12-22 14:33:01 +0100

    Segundo dia de proyecto

--- PROYECTO_API_DISNEY/index.js
+++ PROYECTO_API_DISNEY/index.js
@@ -1,34 +1,74 @@
  
-cont=0
+window.addEventListener('scroll',()=>{
+  if(window.scrollY+window.innerHeight >= document.body.offsetHeight-1000){
+    loadDoc(siguientePagina);
+  }
+});
 
-function loadDoc() {
+cont=0;
+var siguientePagina = "";
+window.onload =inicio;
+
+function loadDoc(url) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
             maquetar(JSON.parse(this.responseText));
-            //search(JSON.parse(this.responseText));
+            siguientePagina = JSON.parse(this.responseText).nextPage;
         }
     };
-    xhttp.open("GET","https://api.disneyapi.dev/characters", true);//DISNEY API
+    xhttp.open("GET",url, true);//DISNEY API
     xhttp.send();
   }
-  
+  function inicio(){
+    loadDoc("https://api.disneyapi.dev/characters");
+  }
 
-  function maquetarCard(id) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-           // maquetar(JSON.parse(this.responseText));
-            //search(JSON.parse(this.responseText));
-            console.log(this.responseText);
-        }
-    };
-    xhttp.open("GET","https://api.disneyapi.dev/characters/"+id, true);//DISNEY API
-    xhttp.send();
+
+
+  function lista(lista){
+    var li = "";
+    if(lista.length > 0){
+      for(let i = 0; i <lista.length; i++){
+         li += lista[i] + ", ";
+      }
+      return li;
+
+    }else{
+      return "-";
+    }
+
+
+  }
+  function maquetarCard(json) {
+    console.log(json);
+    var modal = document.getElementById("tvesModal");
+    var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+
+    document.body.style.position = "static";
+    document.body.style.height = "100%";
+    document.body.style.overflow = "hidden";
+
+    document.getElementById("name").textContent = json.name;
+    document.getElementById("peliculas").textContent="Films: "+lista(json.films);
+    document.getElementById("peliculasCortas").textContent="Short films: "+lista(json.shortFilms);
+    document.getElementById("series").textContent="shows: "+lista(json.tvShows);
+    document.getElementById("videoJuegos").textContent="Video Games: "+lista(json.videoGames);
+    document.getElementById("atracciones").textContent="Atractions: "+lista(json.parkAttractions);
+    document.getElementById("aliados").textContent="Alies: "+lista(json.allies);
+    document.getElementById("enemigos").textContent="Enemies: "+lista(json.enemies);
+    span.onclick = function() {
+        modal.style.display = "none";
+
+        document.body.style.position = "inherit";
+        document.body.style.height = "auto";
+        document.body.style.overflow = "visible";
+    }
   }
 function maquetar(json){
 
-    for(let i=cont;i<json.data.length;i++){
+    for(let i=0;i<json.data.length;i++){
         console.log(json.data[i].name);
         var div= document.createElement("div");
         div.style.width="10rem";
@@ -40,7 +80,7 @@ function maquetar(json){
         div.style.paddingBottom= "2rem";
         div.style.backgroundColor="rgba(215, 189, 226)";
         div.onclick=()=>{
-            maquetarCard(json.data[i]._id);
+            maquetarCard(json.data[i]);
         }
 
         var img=document.createElement("img");
@@ -66,7 +106,3 @@ function maquetar(json){
 }
 }
 
-
-
-
-window.onload =loadDoc;
