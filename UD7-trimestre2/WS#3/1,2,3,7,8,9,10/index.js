ec53b22bb3e1fccc8193a908123e514be167b23c
Rut <rutmariaayusocomino@gmail.com>
2022-01-11 23:05:42 +0100

    enero

--- UD7-trimestre2/WS#3/1,2,3,7,8,9,10/index.js
+++ UD7-trimestre2/WS#3/1,2,3,7,8,9,10/index.js
@@ -47,7 +47,27 @@ $( document ).ready(function() {
     });
 
     //10
-    
+    $(function(){
+        var mayus = new RegExp("^(?=.*[A-Z])");
+        var special = new RegExp("^(?=.*[!@#$%&*])");
+        var numbers = new RegExp("^(?=.*[0-9])");
+        var lower = new RegExp("^(?=.*[a-z])");
+        var len = new RegExp("^(?=.{8,})");
+
+        $("#password").on("keyup",function(){
+            var pass = $("#password").val();
+
+            if(mayus.test(pass) && special.test(pass) &&numbers.test(pass) && lower.test(pass) && len.test(pass)){
+                $('#mensaje').text("SEGURA").css("color", "green");
+
+            }else{
+                $('#mensaje').text("INSEGURA").css("color", "red");
+            }
+        })
+
+
+
+    });
     
 
 
