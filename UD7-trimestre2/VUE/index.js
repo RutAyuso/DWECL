5086a6f53e653e99ad090c8398b48474f012ec3a
Rut <rutmariaayusocomino@gmail.com>
2022-01-15 21:43:37 +0100

    vue

---
+++ UD7-trimestre2/VUE/index.js
@@ -0,0 +1,26 @@
+const Counter = {
+    data() {
+      return {
+        counter: 0,
+        midato : 'Rut',
+        listaNombres : ["jose","pepe","juan"],
+        nuevoNombre: "Escribe tu nombre",
+        listaPersonas:[{nombre:"rut",apellido: "Ayuso Comino",edad:"18"},{nombre:"Manuel",apellido: "Ayuso Terrones",edad:"58"}],
+        listaPersonas2:["eden", "Cristina"],
+        mostrar:false
+    }
+    }
+    ,
+    methods:{
+        incrementa(){
+            console.log("salta");
+            this.counter++;
+            this.listaNombres.push(this.nuevoNombre);
+            this.nombre ="";
+            this.mostrar=true
+        }
+    }
+  }
+  window.onload = () =>{
+  Vue.createApp(Counter).mount('#appVue')
+}
